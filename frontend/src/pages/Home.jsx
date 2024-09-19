dc27228ccbb118a3bc0fa6c8590d7536d8fc7bc4
Mabel Heiner <hei18006@byui.edu>
2024-09-19 17:17:58 -0600

    added theme styling and home page specific styling

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import SearchBook from './SearchBook'
 
+import "./Home.css";
+
 const Home = () => {
   return (
     <div>
         <header>
             <h1>Welcome to Book Hive!</h1>
             <p>🐝<i>Just buzzzzzzzing about books!</i>🐝</p>
-            <p>Want to leave your own reviews? <a href="/login">Login</a></p>
+            <p>Want to leave your own reviews? <button><a href="/login">Login</a></button></p>
         </header>
-        <section>
+        <section className='description-card'>
             <h2>What We Believe</h2>
             <p>We believe in being honest about what material books contain and who should be reading them.</p>
         </section>
-        <section>
+        <section className='description-card'>
             <h2>How We Do This</h2>
             <p>This platform is meant to be a safe place where you can find out more about what books contain before you read them.</p>
         </section>
